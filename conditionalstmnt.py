b50dfcfc6ecc46a535ba287cb8751d2a8e8c773a
Anil-B331 <gyanub331@gmail.com>
2024-12-14 19:45:46 +0545

    Update conditionalstmnt.py

--- conditionalstmnt.py
+++ conditionalstmnt.py
@@ -16,6 +16,10 @@ eat="yes" if food=="masu bhaat" else"no"
 print(eat)
 """
 
-food=input("Food=") # syntax: statement_1 if< condition> else statement_2
-print("Sweet") if food=="cake" or food=="halwa" else print("spicy")
-print("hello world")
+# food=input("Food=") # syntax: statement_1 if< condition> else statement_2
+# print("Sweet") if food=="cake" or food=="halwa" else print("spicy")
+
+#clever if (conditional statement) syntax: var=(false_val,true_val) [<condition>]
+salary=float(input("Enter your salary:"))
+tax=salary*(0.1,0.2) [salary>=50000]
+print("Your tax is:",tax)
