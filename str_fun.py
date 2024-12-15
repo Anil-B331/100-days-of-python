d0494ace7ad572390e57f7c3b2c2a784640ee94b
Anil-B331 <gyanub331@gmail.com>
2024-12-15 20:47:47 +0545

    Create str_fun.py

---
+++ str_fun.py
@@ -0,0 +1,19 @@
+str_1= "Rinkiya"
+str_2= "k"
+str_3= "papa"
+print(str_1 + " " + str_2 +" "+ str_3)
+
+#string functions
+str= "its me learning python"
+print(str.endswith("python"))
+print(str.endswith("leo"))
+print(str.capitalize()) #it will make another variable store those capitalize string
+print(str.replace("me","you"))
+print(str.find("me"))
+print(str.find("python"))
+print(str.count("me"))
+
+# s=input("Enter your first name:")
+# l=len(s)
+# print(l)
+
