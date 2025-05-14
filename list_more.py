4958574e181017c2608d67d7bfe4e505cfbdf20a
Anil-B331 <gyanub331@gmail.com>
2025-05-14 20:19:29 +0545

    Create list_more.py

---
+++ list_more.py
@@ -0,0 +1,21 @@
+import random
+list=['k','xa','tmro','halkhbr']
+# list.append('saathi')
+# list.insert(2,'saathi')
+# print(list)
+# l1=['teri aankho ki namkeen mastiya','teri julfo ki gustak beparwahiya','nahi bhoolunga main','jab tak hain jaan','jab tak hain jaan']
+# print(l1[1])
+# print(l1[random.randint(0,len(l1)-1)])
+
+
+## Deleting
+# using del
+# del(list[2])
+# print(list)
+
+#using pop
+print(list.pop())
+print(list)
+
+list.pop(1)
+print(list)
