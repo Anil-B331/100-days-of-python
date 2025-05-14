2c88939a98057263b2a7a443b5ef9734a1af9f8e
Anil-B331 <gyanub331@gmail.com>
2025-05-14 20:19:40 +0545

    Create list_prac.py

---
+++ list_prac.py
@@ -0,0 +1,7 @@
+list_1=[2,3,11,5,4,45]
+print(f"max:{max(list_1)}")
+print(f"min:{min(list_1)}")
+print(f"sum:{sum(list_1)}")
+print(f"Average:{(sum(list_1)/len(list_1)):.3f}")
+# print(sorted(list_1))
+l 
